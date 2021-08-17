bb3b4248ba8acb0bea5d4b6070bbae4919316801
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-17 09:36:42 -0700

    block_tig

--- include/monkecraft.h
+++ include/monkecraft.h
@@ -1,8 +1,8 @@
 #ifndef MONKECRAFT_H
 #define MONKECRAFT_H
 
-typedef struct block {
-    void new(char type[], float x, float y, float z); // maybe block.new("Dirt", 1,1,1) idk try but nothing will happen
-}
+typedef struct{
+    void new(char type[], float x, float y, float z); // maybe block_t new("Dirt", 1,1,1) idk try but nothing will happen
+} block_t
 
 #endif MONKECRAFT_H
