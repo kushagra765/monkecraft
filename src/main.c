3b2a664aef5d23947206d73588f5ec03d2cee21d
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-16 20:32:40 -0700

    Hmmm

--- src/main.c
+++ src/main.c
@@ -5,6 +5,17 @@ int main(void)
     /* Init */
     InitWindow(800, 450, "Monkecraft");
     SetTargetFPS(60);
+    // Camera
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
+    camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+    
+    // 1st person
+    
+    SetCameraMode(camera, CAMERA_FIRST_PERSON);
 
     /* While esc hasn't been pressed or the close window button hasn't been pressed */
     while (!WindowShouldClose())
@@ -12,13 +23,7 @@ int main(void)
         BeginDrawing();
             ClearBackground(SKYBLUE);
             DrawText("Welcome to Monkecraft!", 190, 200, 20, GREEN);
-            // Camera
-            Camera camera = { 0 };
-            camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
-            camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
-            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };  
-            camera.fovy = 60.0f;
-            camera.projection = CAMERA_PERSPECTIVE;
+            UpdateCamera(&camera); // updates camera
         EndDrawing();
     }
 
