adbf96ae51437f31927bdd1a21a677759a5ac31e
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-17 09:21:32 -0700

    Nothing will happen but hopefully GCC won't throw a big fat fudging fit

---
+++ include/monkecraft.h
@@ -0,0 +1,8 @@
+#ifndef MONKECRAFT_H
+#define MONKECRAFT_H
+
+typedef struct block {
+    void new(char type[], float x, float y, float z); // maybe block.new("Dirt", 1,1,1) idk try but nothing will happen
+}
+
+#endif MONKECRAFT_H
