b97bb6850de17a8ad59aa2429a3099c632f3ffd4
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-16 20:29:11 -0700

    Fixed tabs, changed background. Hopefully works, pls test

--- src/main.c
+++ src/main.c
@@ -10,15 +10,15 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-            ClearBackground(BROWN);
+            ClearBackground(SKYBLUE);
             DrawText("Welcome to Monkecraft!", 190, 200, 20, GREEN);
             // Camera
-        Camera camera = { 0 };
-    camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
-    camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
+            Camera camera = { 0 };
+            camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
+            camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
+            camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };  
+            camera.fovy = 60.0f;
+            camera.projection = CAMERA_PERSPECTIVE;
         EndDrawing();
     }
 
