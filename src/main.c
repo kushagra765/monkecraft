016a7e289fbc70f7ae19b1725a0b6dc3d3f0df38
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-16 19:45:48 -0700

    Hi this is probably broken

--- src/main.c
+++ src/main.c
@@ -12,7 +12,13 @@ int main(void)
         BeginDrawing();
             ClearBackground(BROWN);
             DrawText("Welcome to Monkecraft!", 190, 200, 20, GREEN);
-            //drawCube()
+            // Camera
+        Camera camera = { 0 };
+    camera.position = (Vector3){ 4.0f, 2.0f, 4.0f };
+    camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 60.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
         EndDrawing();
     }
 
