162fb4c1620bc8d8b5bfc8bdc1e14c9a6df1cb68
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-23 09:58:25 -0700

    World size!

--- src/main.c
+++ src/main.c
@@ -25,6 +25,11 @@ int main(void)
     
     // Set exit key to a nonexistant key
     SetExitKey(KEY_NULL);
+    
+    // World size (2x2x2 for now)
+    #define MAX_WIDTH 2 // x
+    #define MAX_HIGHT 2 // y
+    #define MAX_DEPTH 2 // z
 
     /* While esc hasn't been pressed or the close window button hasn't been pressed */
     while (!WindowShouldClose())
