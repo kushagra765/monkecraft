47e183abfee6c1202b6ac8aae49e351a1165ad98
GNUOverYooouu <dtortoise99@gmail.com>
2021-08-25 08:48:35 -0700

    Moved it

--- include/monkecraft.h
+++ include/monkecraft.h
@@ -5,4 +5,9 @@ typedef struct{
     void new(char type[], float x, float y, float z); // maybe block_t new("Dirt", 1,1,1) idk try but nothing will happen
 } block_t
 
+// World size (2x2x2 for now)
+#define MAX_WORLD_WIDTH 2 // x
+#define MAX_WORLD_HIGHT 2 // y
+#define MAX_WORLD_DEPTH 2 // z
+
 #endif MONKECRAFT_H
